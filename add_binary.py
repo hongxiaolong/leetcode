e375bd9579f937e5d6a8226648deb695fa2e4279
hongxiaolong <hongxiaolong.info@gmail.com>
2015-06-06 11:06:03 +0800

    Pay attention to Carry bit.
    Given two binary strings, return their sum (also a binary string).

---
+++ add_binary.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# encoding: utf-8
+import unittest
+
+
+class Solution:
+    # @param {string} a
+    # @param {string} b
+    # @return {string}
+    def addBinary(self, a, b):
+        la, lb = len(a), len(b)
+        pos, flag = 0, 0
+        longer = list(a) if la >= lb else list(b)
+        while pos + la and pos + lb:
+            pos = pos - 1
+            if int(a[pos]) and int(b[pos]):
+                longer[pos] = unicode(flag)
+                flag = 1
+            elif int(a[pos]) or int(b[pos]):
+                longer[pos] = unicode((flag + 1) % 2)
+                flag = flag % 2
+            else:
+                longer[pos] = unicode(flag)
+                flag = 0
+        if flag:
+            ll = len(longer)
+            pos = pos - 1
+            while pos + ll >= 0:
+                if int(longer[pos]):
+                    longer[pos] = u'0'
+                    pos = pos - 1
+                else:
+                    longer[pos] = u'1'
+                    break
+            if pos + ll < 0:
+                longer.insert(0, u'1')
+        return ''.join(longer)
+
+
+class BinaryCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_add_binary(self):
+        s = Solution()
+        for a, b, c in [('', '', ''),
+                        ('', '010', '010'),
+                        ('010', '010', '100'),
+                        ('110', '110', '1100'),
+                        ('110010', '10111', '1001001'),
+                        ('101111', '10', '110001')]:
+            self.assertEqual(s.addBinary(a, b), c)
+
+
+if __name__ == '__main__':
+    unittest.main()
