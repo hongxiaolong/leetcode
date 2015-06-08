d2951a2fd08cf8c2ee96511d6cc562fda9661188
hongxiaolong <hongxiaolong.info@gmail.com>
2015-06-08 09:13:09 +0800

    Pay attention to Carry bit.
    Given a non-negative number represented as an array of digits, plus one
    to the number.

---
+++ plus_one.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# encoding: utf-8
+import unittest
+
+
+class Solution:
+    # @param {integer[]} digits
+    # @return {integer[]}
+    def plusOne(self, digits):
+        if not digits:
+            return []
+        flag = 0
+        for i in xrange(-1, -len(digits) - 1, -1):
+            tmp = digits[i] + flag
+            tmp = tmp if i != -1 else tmp + 1
+            flag, digits[i] = tmp // 10, tmp % 10
+        if flag:
+            digits.insert(0, flag)
+        return digits
+
+
+class PlusOneCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_plus_one(self):
+        s = Solution()
+        for digits, ret in [([], []),
+                            ([0], [1]),
+                            ([1, 9], [2, 0]),
+                            ([9, 9], [1, 0, 0]),
+                            ([9, 0, 9], [9, 1, 0])]:
+            self.assertEqual(s.plusOne(digits), ret)
+
+
+if __name__ == '__main__':
+    unittest.main()
