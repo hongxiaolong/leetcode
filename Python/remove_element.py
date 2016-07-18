e59cf9a34f3500936cf5cff897378dc6bb1661de
hongxiaolong <hongxiaolong.info@gmail.com>
2016-07-18 18:05:04 +0800

    Remove all instances of a value from array and return the new length
    Easily remove elements from Python List using del

---
+++ Python/remove_element.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# encoding: utf-8
+import unittest
+
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        l, r = len(nums), 0
+        for i in xrange(l - 1, -1, -1):
+            if nums[i] == val:
+                del nums[i]
+                r += 1
+        return l - r
+
+
+class RemoveElementCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_add_binary(self):
+        s = Solution()
+        for arr_bef, v, arr_aft, l in [
+            ([3, 2, 2, 3], 3, [2, 2], 2),
+            ([], 3, [], 0),
+            ([3], 2, [3], 1)]:
+            self.assertEqual(s.removeElement(arr_bef, v), l)
+            self.assertListEqual(arr_bef, arr_aft)
+
+
+if __name__ == '__main__':
+    unittest.main()
