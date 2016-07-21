b113dca285c8c516fce1fec411d1f39c9cdfe44e
hongxiaolong <hongxiaolong.info@gmail.com>
2016-07-21 19:39:54 +0800

    Implement a basic calculator to evaluate a simple expression string
    Take care of all cases and gradually improve the code

---
+++ Python/basic_calculator.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# encoding: utf-8
+import unittest
+
+
+class Solution(object):
+    def calculate(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        def subcal(s, p):
+            """
+            :type s: str
+            :type p: int
+            :rtype: (int, int)
+            """
+            r, o = [], 1
+            while(p < len(s)):
+                if (s[p] == ' '):
+                    p = p + 1
+                elif (s[p] == '('):
+                    n, p = subcal(s, p + 1)
+                    r.append(n * o)
+                elif (s[p] == ')'):
+                    break
+                elif (s[p] == '+'):
+                    o = 1
+                    p = p + 1
+                elif (s[p] == '-'):
+                    o = -1
+                    p = p + 1
+                else:
+                    n = []
+                    while(p < len(s) and s[p].isdigit()):
+                        n.insert(0, int(s[p]))
+                        p = p + 1
+                    for i in xrange(len(n)):
+                        n[i] = n[i] * (10 ** i)
+                    r.append(o * reduce(lambda x, y: x + y, n))
+
+            return (reduce(lambda x, y: x + y, r), p + 1)
+
+        return subcal(s, 0)[0]
+
+
+class BasicCalculatorCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_calculate(self):
+        s = Solution()
+        for i, r in [("1 + 1", 2),
+                     (" 2-1 + 2 ", 3),
+                     ("(1+(4+5+2)-3)+(6+8)", 23),
+                     (" 2147483647  ", 2147483647)]:
+            self.assertEqual(s.calculate(i), r)
+
+
+if __name__ == '__main__':
+    unittest.main()
