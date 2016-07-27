08454b45198a7658adb8237f5bd4d8b4a8d0b9ed
hongxiaolong <hongxiaolong.info@gmail.com>
2016-07-27 18:43:58 +0800

    Given an integer n, return the number of trailing zeroes in n!
    A pure mathematical problem

---
+++ Python/factorial_trailing_zeroes.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# encoding: utf-8
+import unittest
+
+
+class Solution(object):
+    def trailingZeroes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        x = 5
+        num = 0
+        while n >= x:
+            num += n / x
+            x *= 5
+        return num
+
+
+class TrailingZeroesCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_add_binary(self):
+        s = Solution()
+        for a, b in [(0, 0),
+                     (1, 0),
+                     (10, 2),
+                     (123456, 30860)]:
+            self.assertEqual(s.trailingZeroes(a), b)
+
+
+if __name__ == '__main__':
+    unittest.main()
