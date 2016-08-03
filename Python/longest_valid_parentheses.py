aece1f46f89d4293f8591630fb3dfb0af985ae7c
hongxiaolong <hongxiaolong@jd.com>
2016-08-03 19:00:05 +0800

    Find the length of the longest valid (well-formed) parentheses substring
    Find the substring with stack

---
+++ Python/longest_valid_parentheses.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# encoding: utf-8
+import unittest
+
+
+class Solution(object):
+    def longestValidParentheses(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        longest = 0
+        stack = []
+        for i in xrange(len(s)):
+            if s[i] == "(":
+                stack.append(("(", i))
+            else:
+                if len(stack) > 0 and stack[len(stack) - 1][0] == "(":
+                    # 可以匹配，将(出栈
+                    stack.pop()
+                    if (len(stack) == 0):
+                        # 如果栈为空，则说明所有括号()正好完整匹配
+                        longest = i + 1
+                    else:
+                        # 如果栈不为空，说明只能部分匹配，该部分匹配的最长长度为当前位置减去栈顶)的位置
+                        longest = max(longest, i - stack[len(stack) - 1][1])
+                else:
+                    # 将)和其位置入栈
+                    stack.append((")", i))
+
+        return longest
+
+
+class LongestValidParenthesesCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_longes_valid_parentheses(self):
+        s = Solution()
+        for a, b in [("", 0),
+                     ("(()", 2),
+                     (")()())", 4),
+                     (")()())((())()())", 10),
+                     ("))))))((((((())))))(((((((((()())((())()())", 16)]:
+            self.assertEqual(s.longestValidParentheses(a), b)
+
+
+if __name__ == '__main__':
+    unittest.main()
